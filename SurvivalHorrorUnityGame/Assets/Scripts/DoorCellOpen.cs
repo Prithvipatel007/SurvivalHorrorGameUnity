79aa3eee5cae088676b5fc83ed5c4e5352dcdf2a
Prithvipatel007 <prithvipatel007@gmail.com>
2021-06-14 00:46:02 +0200

    door opening functionality implemented

---
+++ SurvivalHorrorUnityGame/Assets/Scripts/DoorCellOpen.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCellOpen : MonoBehaviour
+{
+    public float TheDistance;
+
+    [SerializeField]
+    public GameObject ActionDisplay;
+    
+    [SerializeField]
+    public GameObject ActionText;
+
+    [SerializeField]
+    public GameObject TheDoor;
+
+    [SerializeField]
+    public AudioSource CreakSound;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
+
+    void OnMouseOver()
+    {
+        if (TheDistance <= 1)
+        {
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Action"))
+        {
+            if(TheDistance <= 1)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                TheDoor.GetComponent<Animation>().Play("FirstDoorOpenAnim");
+                CreakSound.Play();
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+
+}
---
+++ SurvivalHorrorUnityGame/Assets/Scripts/PlayerCasting.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerCasting : MonoBehaviour
+{
+    public static float DistanceFromTarget;
+
+    public float ToTarget;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit)) // if we find the raycast
+        {
+            ToTarget = Hit.distance;
+            DistanceFromTarget = ToTarget;
+        }
+    }
+}
