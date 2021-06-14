5fd9ff98f1e56868922334d90bc9b0b9d4268695
Prithvipatel007 <prithvipatel007@gmail.com>
2021-06-14 19:02:12 +0200

    firing anim added

---
+++ SurvivalHorrorUnityGame/Assets/Scripts/FirePistol.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePistol : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject TheGun;
+
+    [SerializeField]
+    public GameObject MuzzleFlash;
+
+    [SerializeField]
+    public AudioSource GunFire;
+
+    public bool isFiring = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(isFiring == false)
+            {
+                StartCoroutine(FiringPistol());
+            }
+        }
+    }
+
+    IEnumerator FiringPistol()
+    {
+        isFiring = true;
+        TheGun.GetComponent<Animation>().Play("PistolShotAnim");
+        MuzzleFlash.SetActive(true);
+        MuzzleFlash.GetComponent<Animation>().Play("MuzzleAnim");
+        GunFire.Play();
+        yield return new WaitForSeconds(0.5f);
+        isFiring = false;
+        MuzzleFlash.SetActive(false);
+
+
+    }
+}
